1b64e03293e4bdacb89c5e7d96a76a632208b9e3
Raduha <Radr.shuhart@ya.ru>
2020-08-07 14:04:12 +0600

    Задал описание полей и настроил вывод

---
+++ src/components/InputTemplate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h6 class="named-input__title">{{ title }}</h6>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InputTemplate",
+    props: {
+        title: String
+    }
+}
+</script>
---
+++ src/components/SelectInput.vue
@@ -0,0 +1,11 @@
+<template>
+    <select>
+        
+    </select>
+</template>
+
+<script>
+export default {
+    name: "SelectInput"
+}
+</script>
